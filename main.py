054ce93283ad3ff120a8582e03de99fc5f386049
Abner Saavedra <ingenieroabnersaavedra@bitbucket.org>
2024-06-05 15:35:02 -0400

    Item, Tag class and post item are  added

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
-from typing import Union
+from typing import List, Union
  
 from fastapi import FastAPI
+from pydantic import BaseModel
  
 app = FastAPI()
+
+class Tag(BaseModel):
+    name: str
+    description: str
+
+class Item(BaseModel):
+    id: int
+    name: str
+    description: str
+    tags: List[Tag]
+
+listTags = []
  
 @app.get("/")
 def read_root():
     return {"message": "Hello from Koyeb"}
+
+@app.post("/item")
+async def createItem(item: Item):
+    listTags.append(item)
+    return item
